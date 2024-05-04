5851a52e6495f36f3a3f73322350ee286996669e
AndreasMurk <andimurk@hotmail.de>
2024-05-04 19:04:19 +0200

    refactor(backend): return transcribed text to speech for now

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS
 
 from openai import OpenAI
 import tempfile
 
 from adapters.WhisperService import WhisperService
+from adapters.TextToSpeech import TextToSpeech
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
@@ -25,7 +26,10 @@ def upload_file():
         temp_file.write(audio_file.read())
 
     whisper = WhisperService(OpenAI())
-    return whisper.get_text_from_path(temp_file.name)
+    transcribed_text = whisper.get_text_from_path(temp_file.name)
+    textToSpeech = TextToSpeech(OpenAI())
+    audio_chunks = textToSpeech.get_speech_from_text(transcribed_text)
+    return Response(audio_chunks, mimetype='audio/mpeg')
 
 
 if __name__ == '__main__':
