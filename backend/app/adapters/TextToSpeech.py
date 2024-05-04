3cf8b1ba9f55fa57ebe48919dd7a480f70bbc1ed
AndreasMurk <andimurk@hotmail.de>
2024-05-04 19:04:06 +0200

    refactor(backend): rename

--- backend/app/adapters/TextToSpeech.py
+++ backend/app/adapters/TextToSpeech.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 
-class SpeechToText:
+class TextToSpeech:
     def __init__(self, client):
         self.client = client
-        self.speech_file_path = Path(__file__).parent / "speech.mp3"
 
     def save_speech_from_text(self, text: str):
         return self.get_speech_from_text(text)
@@ -15,4 +14,4 @@ class SpeechToText:
             voice="alloy",
             input=text
         )
-        response.stream_to_file(self.speech_file_path)
+        return response.content
